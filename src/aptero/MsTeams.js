af91d52bcb05eeb700a0fb68c4538b44b6dc5235
PH <pierre.henri@aptero.co>
2024-08-05 14:54:12 +0200

    iframe mode fix + compatibility teams

--- src/aptero/MsTeams.js
+++ src/aptero/MsTeams.js
@@ -1,17 +1,18 @@
 
-export function isInsideIFrame(){
-  return window.self !== window.top;//We assume Iframe === Teams
-  //return window.top.location.href.indexOf("teams.microsoft.com") !== -1;
-  //return true;//used for debug
-}
-
 export function isInsideTeams(){
-  //if we are inside an iframe, we are inside teams (we assume Iframe === Teams)
-  return isInsideIFrame();
+  //return window.self !== window.top;//We assume Iframe === Teams
+  //return window.top.location.href.indexOf("teams.microsoft.com") !== -1;
+  try {
+    return (window.parent.location.href.indexOf("teams") !== -1)
+      || (window.parent.location.href.indexOf("microsoft") !== -1);
+  } catch (e) {
+    return false; // default to not in iframe
+  }
+  //return true;//used for debug
 }
 
 export function isOutsideTeams(){
   //if we are inside an iframe, we are inside teams (we assume Iframe === Teams)
-  return !isInsideIFrame();
+  return !isInsideTeams();
 }
 
